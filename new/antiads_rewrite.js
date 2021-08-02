1aab95b83b399fdc2b299119d9e6b59344251136
ding667 <48525679+ding667@users.noreply.github.com>
2021-08-02 16:46:43 +0800

    Update antiads_rewrite.js

--- new/antiads_rewrite.js
+++ new/antiads_rewrite.js
@@ -105,3 +105,5 @@ https://api.live.bilibili.com/xlive/app-room/v1/index/getInfoByRoom\?access_key 
 ^https?://ios(-.*)?\.prod\.ftl\.netflix\.com/iosui/user/.+path=%5B%22videos%22%2C%\d+%22%2C%22summary%22%5D url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/nf_rating.js
 // [mitm]
 // hostname = ios-*.prod.ftl.netflix.com,ios.prod.ftl.netflix.com
+
+//rsstest
