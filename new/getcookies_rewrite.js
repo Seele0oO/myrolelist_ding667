f2b6a556c78491d425bf9768a59c64fc219a4b5b
ding667 <48525679+ding667@users.noreply.github.com>
2021-08-06 14:10:37 +0800

    Update getcookies_rewrite.js

--- new/getcookies_rewrite.js
+++ new/getcookies_rewrite.js
@@ -1,11 +1,11 @@
 
 // 京东自动签到
 https:\/\/api\.m\.jd\.com\/client\.action.*functionId=signBean url script-request-header https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js
-hostname = api.m.jd.com
+hostname = api.m.jd.com，music.163.com，
 
 //网易云音乐
 ^https:\/\/music.163.com\/weapi\/user\/level url script-request-body https://raw.githubusercontent.com/chavyleung/scripts/master/neteasemusic/neteasemusic.cookie.js
-hostname = music.163.com
+// hostname = music.163.com
 
 // //10086
 // ^https:\/\/clientaccess.10086.cn\/biz-orange\/LN\/uamrandcodelogin\/autoLogin url script-request-body https://raw.githubusercontent.com/chavyleung/scripts/master/10086/10086.fee.cookie.js
@@ -17,7 +17,7 @@ hostname = music.163.com
 // ^http:\/\/wx.10086.cn\/website\/taskCenter\/sign\? url script-request-header https://raw.githubusercontent.com/chavyleung/scripts/master/10086/10086.cookie.js
 
 // hostname = wx.10086.cn
-//米游社
-hostname = api-takumi.mihoyo.com
-^https:\/\/api-takumi.mihoyo.com\/apihub\/api\/getGameList url script-request-header https://raw.githubusercontent.com/chavyleung/scripts/master/mihoyo/mihoyo.cookie.js
+// //米游社
+// hostname = api-takumi.mihoyo.com
+// ^https:\/\/api-takumi.mihoyo.com\/apihub\/api\/getGameList url script-request-header https://raw.githubusercontent.com/chavyleung/scripts/master/mihoyo/mihoyo.cookie.js
 
